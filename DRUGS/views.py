77b16d3ba7ed508ffc91a0218abb4b56d179e201
Luiz Fernando Dezorze <lfernando.dezorze@gmail.com>
2022-12-19 12:03:45 -0300

    header and css formatting

--- DRUGS/views.py
+++ DRUGS/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'DRUGS/home.html')
+    return render(request, 'DRUGS/pages/home.html', context={
+        'drug': 'Dipirona 1g/2ml',
+    })
